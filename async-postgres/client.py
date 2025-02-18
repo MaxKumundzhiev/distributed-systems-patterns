999f0e90ee020363155ae091f8153958e10e8d6c
KumundzhievMaxim <37558223+MaxKumundzhiev@users.noreply.github.com>
2025-02-18 07:34:39 +0500

    connection for async pg client;

---
+++ async-postgres/client.py
@@ -0,0 +1,17 @@
+import asyncio
+import asyncpg
+
+async def main():
+    connection = await asyncpg.connect(
+        host="127.0.0.1",
+        port=5430,
+        user="postgres",
+        database="postgres",
+        password="password"
+    )
+    version = connection.get_server_version()
+    print(f"Connected! Version of Postgres {version}")
+    await connection.close()
+
+
+asyncio.run(main())
