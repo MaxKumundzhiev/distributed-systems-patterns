1caf74330b823b06151906190455408bc14546c6
KumundzhievMaxim <37558223+MaxKumundzhiev@users.noreply.github.com>
2025-02-18 08:00:52 +0500

    SQL statements for db creation;

--- async-postgres/client.py
+++ async-postgres/client.py
@@ -1,6 +1,16 @@
 import asyncio
 import asyncpg
 
+from statements import (
+    CREATE_PRODUCT_TABLE,
+    CREATE_BRAND_TABLE,
+    CREATE_PRODUCT_COLOR_TABLE,
+    CREATE_PRODUCT_SIZE_TABLE,
+    CREATE_SKU_TABLE,
+    SIZE_INSERT,
+    COLOR_INSERT
+)
+
 async def main():
     connection = await asyncpg.connect(
         host="127.0.0.1",
@@ -11,7 +21,22 @@ async def main():
     )
     version = connection.get_server_version()
     print(f"Connected! Version of Postgres {version}")
-    await connection.close()
 
+
+    statements = [
+        CREATE_BRAND_TABLE,
+        CREATE_PRODUCT_TABLE,
+        CREATE_PRODUCT_SIZE_TABLE,
+        CREATE_PRODUCT_COLOR_TABLE,
+        CREATE_SKU_TABLE,
+        SIZE_INSERT,
+        COLOR_INSERT
+    ]
+    print("creating db tables")
+    for statement in statements:
+        status = await connection.execute(statement)
+        print(status)
+    print("db tables created")
+    await connection.close()
 
 asyncio.run(main())
