dbe9f76107958a847c66d3f827ce806ff5c2d489
KumundzhievMaxim <37558223+MaxKumundzhiev@users.noreply.github.com>
2025-02-24 09:25:21 +0500

    custom async generator integrated with cursor;

--- async-postgres/cursor.py
+++ async-postgres/cursor.py
@@ -35,4 +35,24 @@ async def move_cursor():
     await connection.close()
 
 
-asyncio.run(move_cursor())
+async def take(generator, to_take: int):
+    item_count = 0
+    async for item in generator:
+        if item_count > to_take-1:
+            return
+        else:
+            item_count += 1
+            yield item
+
+
+async def move_cursor_with_custom_async_generator():
+    conn = await get_connection()
+    query = "SELECT product_id, product_name FROM product"
+    async with conn.transaction():
+        product_generator = conn.cursor(query)
+        async for product in take(product_generator, 10):
+            print(product)
+    await conn.close()
+
+
+asyncio.run(move_cursor_with_custom_async_generator())
