2778320e4327063c27ad2dbb6b53c21433dc69b7
KumundzhievMaxim <37558223+MaxKumundzhiev@users.noreply.github.com>
2025-05-04 16:43:50 +0500

    сircuit-breaker caller service;

---
+++ сircuit-breaker/service.py
@@ -0,0 +1,32 @@
+from time import time 
+from uuid import UUID, uuid4
+
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+
+class Request(BaseModel):
+    callee_identifier: str
+
+
+class Response(BaseModel):
+    callee_identifier: str
+    request_id: UUID
+    request_at: int
+    status_code: int
+    circuit_breaker_status: str
+    circuit_breaker_details: dict
+
+
+server = FastAPI(title="service")
+
+@server.post("/", response_model=Response)
+async def post_to_callee(request: Request):
+    return Response(
+        **request.model_dump(),
+        request_id=uuid4(),
+        request_at=int(time()),
+        status_code=200,
+        circuit_breaker_status="CLOSED",
+        circuit_breaker_details={}
+    )
